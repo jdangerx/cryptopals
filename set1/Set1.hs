cae83a57ebc1dac4229d78b6a4aeeadab2bcc868
John Xia <john.danger.xia@gmail.com>
2015-10-09 17:47:51 -0400

    detecting xor but really slow

--- set1/Set1.hs
+++ set1/Set1.hs
@@ -8,6 +8,7 @@ import qualified Data.Map as M
 import Data.Maybe (mapMaybe)
 import Numeric (readHex, showHex)
 
+-- Challenge 1 - hex to b64
 hexTo6BitChunks :: String -> [Int]
 hexTo6BitChunks (a:b:c:xs) =
   (++ hexTo6BitChunks xs) . (\i -> [i `div` 64, i `mod` 64]) . fst . head . readHex $ [a,b,c]
@@ -30,6 +31,7 @@ testHexToB64 =
   hexToB64 "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
   == "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t"
 
+-- Challenge 2: fixed length XOR
 hexToInts :: Char -> Int
 hexToInts = fst . head . readHex . (:[])
 
@@ -49,6 +51,7 @@ testFixedXor :: Bool
 testFixedXor =
   fixedXor xorA xorB == "746865206b696420646f6e277420706c6179"
 
+-- Challenge 3: 1-byte XOR cipher
 byteXor :: String -> Int -> String
 byteXor s i =
   let leftHex = showHex (i `mod` 16) ""
@@ -62,11 +65,33 @@ letters = M.fromList (zip "etaoinsrhdlucmfywgpbvkxqjz" (reverse [0..25]))
 score :: String -> Int
 score s = sum . mapMaybe (`M.lookup` letters) $ s
 
-sbXorCipher :: String -> [(String, Char)]
-sbXorCipher s =
-  sortOn (score . fst) $
-  (\(hex, c) -> (hexToChars hex, c)) . (\i -> (byteXor s i, chr i)) <$> [0..255]
-
 hexToChars :: String -> String
 hexToChars (a:b:bs) = (chr . fst . head . readHex $ [a,b]) : hexToChars bs
 hexToChars _ = []
+
+sbXorCipher :: String -> [(String, Char)]
+sbXorCipher s =
+  take 1 . reverse . sortOn (score . fst) $
+  (\(hex, c) -> (hexToChars hex, c)) . (\i -> (byteXor s i, chr i)) <$> [0..255]
+
+sbXorCipher_ :: String -> String
+sbXorCipher_ s =
+  last . sortOn score $ hexToChars . byteXor s <$> [0..255]
+
+testSbXorCipher :: Bool
+testSbXorCipher =
+  (fst . head . sbXorCipher $ "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+  == "Cooking MC's like a pound of bacon"
+
+-- Challenge 4: detect 1-byte XOR cipher
+detectXor :: [String] -> String
+detectXor ss = fst . last . sortOn (score . fst) . concatMap sbXorCipher $ ss
+
+testDetectXor :: IO Bool
+testDetectXor =
+  do
+    inp <- readFile "4.txt"
+    let result = detectXor . lines $ inp
+    return $ result == "Now that the party is jumping\n"
+
+main = testDetectXor >>= print
